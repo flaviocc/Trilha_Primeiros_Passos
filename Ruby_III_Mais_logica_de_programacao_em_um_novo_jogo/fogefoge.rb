57381c8ce25e0649de680370eef062ba6765453e
Flávio Lopes <flaviolopes.cc@gmail.com>
2016-07-22 00:15:35 -0300

    Correção

--- Ruby_III_Mais_logica_de_programacao_em_um_novo_jogo/fogefoge.rb
+++ Ruby_III_Mais_logica_de_programacao_em_um_novo_jogo/fogefoge.rb
@@ -4,7 +4,7 @@ require_relative 'logic'
 def joga(nome)
   mapa = le_mapa(1)
   while true
-    desenha mapa =>
+    desenha mapa
     direcao = pede_movimento
     heroi = encontrar_jogador mapa
     nova_posicao = calcula_nova_posicao heroi, direcao
